0ad12bfef36ec5031a898504081bc79a66f34f81
Ivan-Loz <vebikt@gmail.com>
2025-12-15 19:51:51 +0300

    Мой C--

---
+++ ConsoleApplication33/ConsoleApplication33/ConsoleApplication33.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+class State;
+class Door {
+public:
+    Door(State* initialState) : currentState(initialState) {}
+
+    void setState(State* state) {
+        currentState = state;
+    }
+
+    void openn();
+    void closee();
+private:
+    State* currentState;
+};
+class State {
+public:
+    virtual void open(Door* dor) = 0;
+    virtual void close(Door* dor) = 0;
+};
+class OpenState : public State {
+public:
+    void open(Door* dor) {
+        cout << "Door open" << endl;
+        dor->setState(new ClosedState());
+    }
+};
+class ClosedState : public State {
+public:
+    void close(Door* dor) {
+        cout << "Door close" << endl;
+        dor->setState(new OpenState());
+    }
+};
+int main() {
+    OpenState dddd;
+    Door dor(new OpenState());
+}
